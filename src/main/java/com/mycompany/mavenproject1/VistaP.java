4b851c34f5b7a6c778f68120bb362cdf04dddfb0
Alfonso28 <alfonsoacosta207@gmail.com>
2026-02-19 16:44:24 -0600

    prueba

--- src/main/java/com/mycompany/mavenproject1/VistaP.java
+++ src/main/java/com/mycompany/mavenproject1/VistaP.java
@@ -8,7 +8,6 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
